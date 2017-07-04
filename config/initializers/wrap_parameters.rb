e5d9f72d28c7ff2262a5d39bf322cc1330ff35a5
Sybil <sybil.deboin@gmail.com>
2017-07-03 23:48:58 -0400

    Add root in serializers

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -8,6 +8,8 @@
 # end
 
 # To enable root element in JSON for ActiveRecord objects.
-# ActiveSupport.on_load(:active_record) do
-#  self.include_root_in_json = true
-# end
+ActiveModelSerializers.config.adapter = :json
+
+ActiveSupport.on_load(:active_record) do
+ self.include_root_in_json = true
+end
